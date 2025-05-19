56018ea0bf7dcc53ae5d70218f4f16d84bc6bfaa
albertorcf <albertorcf@gmail.com>
2025-05-19 14:15:20 -0300

    Add JSON visualization area for CustomValueEditor output in QueryBuilderPage

--- src/app/query-builder/page.tsx
+++ src/app/query-builder/page.tsx
@@ -101,6 +101,16 @@ export default function QueryBuilderPage() {
           />
         </div>
       </div>
+      {/* Área de visualização do JSON gerado */}
+      <div className="flex flex-col lg:flex-row gap-4 mt-1">
+        {/* Coluna 1: JSON query */}
+        <div className="flex-1">
+          <h2 className="text-lg font-semibold mb-1">CustomValueEditor test:</h2>
+          <pre className="bg-gray-100 p-4 rounded text-xs">
+            {JSON.stringify(query, null, 2)}
+          </pre>
+        </div>
+      </div>
 
       <div className="flex flex-col lg:flex-row gap-6 mb-4 h-37 w-full">
 
