3cd9023658cf35cf1e45535277c670ce07a729a0
albertorcf <albertorcf@gmail.com>
2025-05-19 18:11:34 -0300

    Add debug log for fieldData datatype in CustomValueEditor

--- src/components/query-builder/CustomValueEditor.tsx
+++ src/components/query-builder/CustomValueEditor.tsx
@@ -7,6 +7,7 @@ import { ValueEditor, ValueEditorProps } from 'react-querybuilder';
 const dateFormat = 'yyyy-MM-dd';
 
 export const CustomValueEditor = (props: ValueEditorProps) => {
+  //console.log('props.fieldData.datatype=', props.fieldData.datatype);
   if (props.fieldData.datatype === 'function') {
     return (<></>);
   } else if (props.fieldData.datatype === 'date') {
